a5bd4dfd65ceff11ec61a275270eed2a8d96b306
kristina <kistina@localhost>
2021-07-12 22:57:58 +0300

    init done

---
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -0,0 +1,10 @@
+import GameSavingLoader from '../GameSavingLoader';
+
+const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}';
+
+test('should return', (done) => {
+  GameSavingLoader.load().then((received) => {
+    expect(received).toBe(expected);
+  }).catch((error) => error);
+  done();
+});
